5aa2c9f0a658fe32922fb94deabaac858768f4dc
Victor Ramos <victorrms03@gmail.com>
2025-04-06 17:52:26 -0300

    feat: Hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,8 @@ import Image from 'next/image';
 export default function Header() {
     return (
         <header className="w-full px-6 py-4 bg-white shadow-md sticky top-0 z-50">
-            <div className="max-w-7xl mx-auto flex items-center justify-between">
+            <div className="max-w-9/12 mx-auto flex items-center justify-between">
 
-                {/* aqui fica a logo */}
                 <div className="flex items-center gap-3 text-xl font-bold text-black">
 
                     <Image src="/images/Foto - Victor Ramos.jpg" 
@@ -19,21 +18,19 @@ export default function Header() {
                 
                 </div>
         
-                {/* aqui ficam os itens do meio */}
                 <nav className="hidden md:flex gap-6 text-black font-medium">
-                    <a href="#inicio" className="hover:text-grey-600 transition">Sobre mim</a>
+                    <a href="#inicio" className="hover:text-grey-800 transition">Sobre mim</a>
                     <a href="#sobre" className="hover:text-grey-800 transition">Habilidades</a>
                     <a href="#projetos" className="hover:text-grey-800 transition">Experiências</a>
                     <a href="#contato" className="hover:text-grey-800 transition">Contato</a>
                 </nav>
         
-                {/* aqui fica o botão à direita */}
                 <div className="hidden md:block">
 
                     <a href="/archives/Curriculum Vitae - Victor Ramos.pdf" target="_blank">
 
                         <button className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition flex items-center gap-2">
-                            Currículo <Image src="/icons/download-icon.svg" alt="icone download" width={15} height={15}/>
+                            Currículo <Image src="/icons/download.svg" alt="icone download" width={15} height={15}/>
                         </button>
 
                     </a>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,58 @@
+import Image from "next/image";
+
 export default function Hero() {
     return (
-        <div>
-            <h1>HERO</h1>
-        </div>
+
+        <section className="mt-20">
+
+            <div className="max-w-9/12 mx-auto px-6 flex flex-col md:flex-row items-center justify-between gap-12">
+
+                <div className="w-1/2">
+
+                    <h2 className="text-5xl mb-3"> Olá, eu sou <span className="font-extrabold">Victor Ramos!</span> </h2>
+                    <h2 className="text-5xl mb-6"> <span className="font-extrabold">Desenvolvedor</span> Fullstack. </h2>
+
+                    <p> Estudante da área de Computação desde 2019. Sempre apaixonado por programação e afins, 
+                        busco agora oportunidades de colocar em prática meus conhecimentos obtidos nos últimos 
+                        anos e acrescentar valor por onde passar, enquanto continuo a aprender mais e mais!
+                    </p>
+
+                </div>
+
+                <div>
+
+                    <Image src="/images/devPicture.svg" alt="Ilustração de dev" width={600} height={600}/>
+
+                </div>
+
+            </div>
+
+            <div className="w-full flex justify-center items-center gap-12 mt-16">
+
+                <a href="https://www.instagram.com/victor_rms01/" target="_blank" rel="noopener noreferrer" 
+                    className="p-4 rounded bg-white border border-black">
+
+                    <Image src={"/icons/instagram.svg"} alt="icone instagram" width={30} height={30}/>
+
+                </a>
+
+                <a href="https://www.linkedin.com/in/victor-ramos3/" target="_blank" rel="noopener noreferrer"
+                    className="p-4 rounded bg-white border border-black" >
+
+                    <Image src={"/icons/linkedin.svg"} alt="icone instagram" width={30} height={30}/>
+
+                </a>
+
+                <a href="https://github.com/VictorRms03" target="_blank" rel="noopener noreferrer"
+                    className="p-4 rounded bg-white border border-black">
+
+                    <Image src={"/icons/github.svg"} alt="icone instagram" width={30} height={30}/>
+
+                </a>
+                
+            </div>
+
+        </section>
+
     )
 }
