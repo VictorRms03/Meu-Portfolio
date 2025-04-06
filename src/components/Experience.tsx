583d5957b5a00c6c20bf9b426b5b792dfc78bdea
Victor Ramos <victorrms03@gmail.com>
2025-04-06 20:18:50 -0300

    feat: Experiências

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,18 @@
+import ExperienceList from "./Experience/ExperienceList";
+
 export default function Experience() {
     return (
-        <div>
-            <h1>EXPERIENCE</h1>
-        </div>
+        
+        <section className="py-16 bg-black text-white">
+
+            <div className="max-w-9/12 mx-auto flex justify-center">
+
+                <h2 className="text-5xl"> Experiências </h2>
+
+            </div>
+
+            <ExperienceList />
+
+        </section>
     )
 }
