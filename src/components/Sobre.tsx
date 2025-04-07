86539ae87b322f7ab2eae833606b9167b07c87f0
Victor Ramos <victorrms03@gmail.com>
2025-04-06 22:52:18 -0300

    feat: texto de 'Sobre mim'

--- src/components/Sobre.tsx
+++ src/components/Sobre.tsx
@@ -17,19 +17,15 @@ export default function Sobre() {
 
                     <h2 className="text-5xl"> Sobre <span className="font-extrabold">Mim!</span> </h2>
 
-                    <p className="mt-12"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nunc lacus, accumsan eget purus eget,
-                         dapibus elementum ipsum. Nulla porttitor lacus ut consectetur sagittis. Sed non pharetra dolor. Aenean
-                         iaculis ante ac dui viverra pretium. Aenean fermentum lorem in sapien sollicitudin, vitae volutpat dolor
-                         dignissim. Nulla victus volutpat. Nam vel neque vel ipsum egestas tincidunt. Fusce quis condimentum mauris.
-                         In faucibus feugiat ligula sed tincidunt. Integer a.
+                    <p className="mt-12"> Olá! Tenho 20 anos e programo desde os 14 quando entrei para o curso técnico em informática,
+                         desde então venho estudo e me apaixonando cada vez mais pela área da tecnologia.
+                         Estou finalizando minha formação como Bacharel em Ciência da Computação e também sou formado em Técnico em Informática para Internet, 
+                         o que me deu uma base sólida quanto ao desenvolvimento tanto Back-end quanto Front-end. Gosto de explorar novas linguagens e ferramentas e
+                         tenho facilidade em aprender coisas novas e estou sempre em busca de novos desafios.
                     </p>
 
-                    <p className="mt-12">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nunc lacus, accumsan eget purus eget,
-                         dapibus elementum ipsum. Nulla porttitor lacus ut consectetur sagittis. Sed non pharetra dolor. Aenean
-                         iaculis ante ac dui viver Etiam cursus nunc velit, eget elementum lectus efficitur ultricies.
-                         Maecenas vehicula nulla et luctus volutpat. Nam vel neque vel ipsum egestas tincidunt. Fusce quis condimentum mauris.
-                         In faucibus feugiat ligula sed tincidunt. Integer a.
+                    <p className="mt-12"> Além da programação, sou uma pessoa comunicativa que gosta de conversas sobre basicamente qualquer assunto!
+                         Entre meus principais hobbies estão ouvir música e jogos, o que ajuda a recarregar as energias e manter a criatividade em dia.
                     </p>
 
                 </div>
