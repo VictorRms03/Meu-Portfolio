bd25a986e782fb2fdf66df9c96a74913f873d9c2
Victor Ramos <victorrms03@gmail.com>
2025-04-07 19:12:53 -0300

    feat: Contato

--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -1,7 +1,35 @@
+import Image from 'next/image';
+import ContatoSocials from './Contato/ContatoSocials';
+
 export default function Contato() {
     return (
-        <div id="contato">
-            <h1>CONTATO</h1>
-        </div>
+
+        <section id="contato" className="flex maw-w-9/12 mx-auto py-30">
+
+            <div className="mx-auto px-6 flex flex-col md:flex-row items-center justify-between gap-24">
+
+                <div className="w-1/2">
+
+                    <Image src="/images/helloPicture.svg" alt="Ilustração de dev" width={900} height={900}/>
+
+                </div>
+
+                <div className="w-1/2">
+
+                    <h2 className="text-5xl mb-3"> Entre em <span className="font-extrabold">Contato!</span> </h2>
+
+                    <p className="font-light"> Estou sempre aberto a novas oportunidades e desafios como desenvolvedor! </p>
+
+                    <ContatoSocials />
+
+                    <h2 className="text-3xl mb-3 font-bold"> victorrms03@gmail.com </h2>
+                    <h2 className="text-3xl mb-3 font-bold"> +55 (19) 99587-3557 </h2>
+
+                </div>
+
+            </div>
+
+        </section>
+
     )
 }
---
+++ src/components/Contato/ContatoSocials.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+export default function ContatoSocials() {
+    return (
+
+        <div className="w-full flex gap-12 py-8">
+                
+            <a href="https://wa.me/19995873557" target="_blank" rel="noopener noreferrer" 
+             className="group p-4 rounded bg-white border-2 border-black hover:bg-black">
+
+                <Image src={"/icons/whatsapp.svg"} alt="icone whatsapp" width={30} height={30} className="group-hover:invert"/>
+
+            </a>
+
+            <a href="https://www.instagram.com/victor_rms01/" target="_blank" rel="noopener noreferrer" 
+                className="group p-4 rounded bg-white border-2 border-black hover:bg-black">
+
+                <Image src={"/icons/instagram.svg"} alt="icone instagram" width={30} height={30} className="group-hover:invert"/>
+
+            </a>
+        
+            <a href="https://www.linkedin.com/in/victor-ramos3/" target="_blank" rel="noopener noreferrer"
+                className="group p-4 rounded bg-white border-2 border-black hover:bg-black">
+
+                <Image src={"/icons/linkedin.svg"} alt="icone linkedin" width={30} height={30} className="group-hover:invert"/>
+
+            </a>
+        
+            <a href="https://github.com/VictorRms03" target="_blank" rel="noopener noreferrer"
+                className="group p-4 rounded bg-white border-2 border-black hover:bg-black">
+
+                <Image src={"/icons/github.svg"} alt="icone github" width={30} height={30} className="group-hover:invert"/>
+
+            </a>
+
+            <a href="https://discordapp.com/users/victorrms" target="_blank" rel="noopener noreferrer"
+                className="group p-4 rounded bg-white border-2 border-black hover:bg-black">
+
+                <Image src={"/icons/discord.svg"} alt="icone discord" width={30} height={30} className="group-hover:invert"/>
+
+            </a>
+                    
+        </div>
+
+    )
+}
