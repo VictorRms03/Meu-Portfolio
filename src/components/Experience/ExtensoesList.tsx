4fbfb1776628e90b6ea9eefbd05fe3cc43adb31e
Victor Ramos <victorrms03@gmail.com>
2025-04-10 20:06:17 -0300

    refactor: construção de experiencia simplificada

--- src/components/Experience/ExtensoesList.tsx
+++ src/components/Experience/ExtensoesList.tsx
@@ -3,70 +3,49 @@ export default function ExtensoesList() {
 
         <div className="max-w-9/12 mx-auto gap-6 py-12 ">
 
-            <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
+            { gerarExtensao( "Iniciação Ciêntifica - Desenvolvimento de Aplicativo para auxílio no projeto e no uso de medidores de vazão de fluidos", 
+                "Fev 2025 - Dez 2025", 
+                "Desenvolvimento de aplicativo Android, Estudo de Medidores de Vazão por dispositivos de pressão diferencial, " + 
+                "Modelagem e Projeto do aplicativo, Projeto e Aplicação de Testes, Documentação Ágil.") }
 
-                <div className="flex justify-between">
+            { gerarExtensao( "Bolsa de Extensão - Desenvolvedor Web para o Conselho do Café da Região de Pinhal (COCAMPI)", 
+                "Jun 2024 - Jan 2025", 
+                "Desenvolvimento do Website, Apuramento de Requisitos, Resolução de Problemas, " +
+                "Condutor de Reuniões e Comunicação com outras empresas para Resolução de Problemas.") }
 
-                    <h3 className="text-x1">Iniciação Ciêntifica - Desenvolvimento de Aplicativo para auxílio no projeto e
-                    no uso de medidores de vazão de fluidos</h3>
-                    <span className="text-gray-400">Fev 2025 - Dez 2025</span>
+            { gerarExtensao( "Diretor de Eventos - Associação Acadêmica Atlética Arthur Chiodi", 
+                "Jan 2023 - Dez 2023", 
+                "Organizador de Eventos, Busca de Locais, Resolução de problemas e Gerênciador de Logistica.") }
 
-                </div>
-
-                <div className="mt-4 max-w-8/12">
-                    <p className="font-extralight"> <span className="font-bold">Principal atividades:</span> Desenvolvimento de aplicativo Android, Estudo de Medidores de
-                         Vazão por dispositivos de pressão diferencial, Modelagem e Projeto do aplicativo, Projeto e Aplicação de Testes,
-                          Documentação Ágil.</p>
-                </div>
-            </div>
-
-            <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
-
-                <div className="flex justify-between">
-
-                    <h3 className="text-x1">Bolsa de Extensão - Desenvolvedor Web para o Conselho do Café da Região de Pinhal (COCAMPI)</h3>
-                    <span className="text-gray-400">Jun 2024 - Jan 2025</span>
-
-                </div>
-
-                <div className="mt-4 max-w-8/12">
-                    <p className="font-extralight"> <span className="font-bold">Principal atividades:</span> Desenvolvimento do Website, Apuramento de Requisitos,
-                         Resolução de Problemas, Condutor de Reuniões e Comunicação com outras empresas para Resolução de Problemas.</p>
-                </div>
-            </div>
-
-            <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
-
-                <div className="flex justify-between">
-
-                    <h3 className="text-x1">Diretor de Eventos - Associação Acadêmica Atlética Arthur Chiodi</h3>
-                    <span className="text-gray-400">Jan 2023 - Dez 2023</span>
-
-                </div>
-
-                <div className="mt-4 max-w-8/12">
-                    <p className="font-extralight"> <span className="font-bold">Principal atividades:</span> Organizador de Eventos, Busca de Locais,
-                         Resolução de problemas e Gerênciador de Logistica.
-                    </p>
-                </div>
-            </div>
-
-            <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
-
-                <div className="flex justify-between">
-
-                    <h3 className="text-x1">Bolsa de Ensino - Estudo e Desenvolvimento para Portal de Ensino Web</h3>
-                    <span className="text-gray-400">Fev 2023 - Dez 2023</span>
-
-                </div>
-
-                <div className="mt-4 max-w-8/12">
-                <p className="font-extralight"> <span className="font-bold">Principal atividades:</span> Pesquisa, Estudo, Testes e Documentação de cursos
-                 e ferramentas básicas para aprendizado de Desenvolvimento, Documentação destas ferramentas</p>
-                </div>
-            </div>
+            { gerarExtensao( "Bolsa de Ensino - Estudo e Desenvolvimento para Portal de Ensino Web", 
+                "Fev 2023 - Dez 2023", 
+                "Pesquisa, Estudo, Testes e Documentação de cursos e ferramentas básicas para aprendizado " + 
+                "de Desenvolvimento, Documentação destas ferramentas.") }
 
         </div>
+
+    )
+}
+
+function gerarExtensao(titulo: string, data: string, principaisAtividades: string) {
+
+    return (
+
+        <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
+
+                <div className="flex justify-between">
+
+                    <h3 className="text-x1">{titulo}</h3>
+                    <span className="text-gray-400">{data}</span>
+
+                </div>
+
+                <div className="mt-4 max-w-8/12">
+
+                <p className="font-extralight"> <span className="font-bold">Principais atividades:</span> {principaisAtividades} </p>
+
+                </div>
+            </div>
 
     )
 }
--- src/components/Experience/FormacoesList.tsx
+++ src/components/Experience/FormacoesList.tsx
@@ -1,38 +1,29 @@
 export default function FormacoesList() {
+
     return (
 
         <div className="max-w-9/12 mx-auto gap-6 py-12 ">
 
-            <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
+            { gerarFormacao("Bacharelado em Ciência da Computação - Instuto Federal de São Paulo (IFSP)", "Mar 2022 - Presente (Previsão: Jan 2026)") }
 
-                <div className="flex justify-between">
+            { gerarFormacao("Técnico em Informática para Internet - Escola Técnica Estadual (ETEC)", "Jan 2019 - Dez 2021") }
 
-                    <h3 className="text-x1">Bacharelado em Ciência da Computação - Instuto Federal de São Paulo (IFSP)</h3>
-                    <span className="text-gray-400">Mar 2022 - Presente (Previsão: Jan 2026)</span>
+        </div>
 
-                </div>
+    )
+    
+}
 
-            </div>
+function gerarFormacao(formacao: string, data: string) {
 
-            <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
+    return (
 
-                <div className="flex justify-between">
+        <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
 
-                    <h3 className="text-x1">Análise e Desenvolvimento de Sistemas - Centro Universitário da Fundação de Ensino Octávio Bastos (UNIFEOB)</h3>
-                    <span className="text-gray-400">Jan 2022 - Mar 2022</span>
+            <div className="flex justify-between">
 
-                </div>
-
-            </div>
-
-            <div className="mb-12 border-1 border-white rounded-lg bg-black hover:bg-neutral-800 p-7">
-
-                <div className="flex justify-between">
-
-                    <h3 className="text-x1">Técnico em Informática para Internet - Escola Técnica Estadual (ETEC)</h3>
-                    <span className="text-gray-400">Jan 2019 - Dez 2021</span>
-
-                </div>
+                <h3 className="text-x1">{formacao}</h3>
+                <span className="text-gray-400">{data}</span>
 
             </div>
 
